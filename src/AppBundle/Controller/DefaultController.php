dec5d94aa225d53f3da2abaaa619a1ad5a18cc53
German Nemes <gnemes@Germans-MacBook-Pro.local>
2016-04-09 18:21:00 -0300

    Basic changes to controller and template skeleton

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -51,7 +51,7 @@ class DefaultController extends Controller
     {
         
         return $this->render(
-            'musicstore/index.html.twig', 
+            'AppBundle:musicstore:index.html.twig', 
             array(
                 'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'
             ),
