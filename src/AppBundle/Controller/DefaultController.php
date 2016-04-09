473a4ddf6b4909c4c113bf7adf6a7da4d2fbf3a3
German Nemes <gnemes@Germans-MacBook-Pro.local>
2016-04-09 18:17:51 -0300

    Basic changes to controller and template skeleton

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -1,4 +1,30 @@
 <?php
+/**
+ * Gnemes Music Store
+ * Copyright (C) 2016  German Nemes
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * PHP VERSION 5.6
+ *
+ * @category  Ecommerce
+ * @package   MusicStore
+ * @author    German Nemes <gnemes@gmail.com>
+ * @copyright 2016 German Nemes
+ * @license   GNU GPL v3
+ * @link      https://github.com/gnemes/summasolutions
+ */
 
 namespace AppBundle\Controller;
 
@@ -6,6 +32,16 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Default controller
+ *
+ * @category  Ecommerce
+ * @package   MusicStore
+ * @author    German Nemes <gnemes@gmail.com>
+ * @copyright 2016 German Nemes
+ * @license   GNU GPL v3
+ * @link      https://github.com/gnemes/summasolutions
+ */
 class DefaultController extends Controller
 {
     /**
@@ -13,9 +49,12 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+        
+        return $this->render(
+            'musicstore/index.html.twig', 
+            array(
+                'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'
+            ),
         ));
     }
 }
